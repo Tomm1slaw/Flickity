6a161bbdca074a3b2a45978ef991d0423ff54202
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-08-03 18:33:12 +0200

    Changes map

--- js/script.js
+++ js/script.js
@@ -40,13 +40,13 @@ flkty.on('scroll', function(progress) {
 
 // map
 // Initialize and add the map
-window.initMap = function initMap() {
+window.initMap =  function() {
     
-    var coordinations = sliderData[1].coords;
+    var coordinations = sliderData[i].coords;
     
     var map = new google.maps.Map(
         document.getElementById('map'), {
-            zoom: 5,
+            zoom: 3,
             center: coordinations
         });
     var marker = new google.maps.Marker({
